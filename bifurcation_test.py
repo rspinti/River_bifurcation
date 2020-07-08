f11099aa4dda2687d8ab708424e7d49e0ead5773
Laura Condon <lecondon@email.arizona.edu>
2020-07-07 21:06:25 -0700

    updating bifurcation calcs

--- bifurcation_test.py
+++ bifurcation_test.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# %%
 import pandas as pd
 import numpy as np
 import geopandas as gp
@@ -7,7 +7,7 @@ from shapely import wkt
 plt.style.use('classic')
 
 # %%
-# Read ub tge csv and set Hydroseq as the index
+# Read in the csv and set Hydroseq as the index
 test = pd.read_csv("small1019.csv", index_col='Hydroseq',
                    usecols=['Hydroseq', 'UpHydroseq', 'DnHydroseq',
                             'Pathlength', 'LENGTHKM', 'StartFlag',
@@ -21,6 +21,9 @@ test.insert(5, "step", np.zeros(len(test)), True)
 test['DamID'] = test['DamID'].fillna(0)
 # Copying over the dam IDs into a new fragment column
 test['Frag'] = test['DamID']
+# make a column to indicate if a dam is present or not
+test['DamCount'] = np.zeros(len(test))
+test.loc[test.DamID>0, 'DamCount'] = 1
 
 
 # %%
@@ -34,10 +37,13 @@ test2Geo = gp.GeoDataFrame(test2, geometry='Coordinates')
 segments = test2Geo.copy()
 
 # %%
-# make a data frame for the fragments
-frag_list = segments.DamID.unique()
-flength = np.zeros(len(frag_list))
-fragments = pd.DataFrame(data={'flength': flength}, index=frag_list)
+""" ## Dont need to do this anymore
+## make a data frame for the fragments
+#frag_list = segments.DamID.unique()
+##flength = np.zeros(len(frag_list))
+#fragments = pd.DataFrame(data={'flength': np.zeros(len(frag_list)), 
+#    'Ndam': np.zeros(len(frag_list)),
+#    'DnSeg': np.zeros(len(frag_list))}, index=frag_list) """
 
 # %%
 # looping to make fragments
@@ -48,20 +54,20 @@ queue = segments.loc[segments.UpHydroseq == 0]
 # Initail number to use for fragments that are existing the  domain
 # Rather than hitting a dam. Exiting framents will start counting from
 # this number
-fexit = 0
+fexit = 11
 # for ind in range(len(queue)):
 # for ind in range(len(queue)):
 snum=0  #Counter for the segment starting points -- just for print purposes
 while len(queue) > 0:
-
     # Initialiazation for starting segment:
     step = 0  # start a counter for steps down the fragment
     snum = snum + 1
     temploc = queue.index[0]  # start with the segment at the top of the queue
     tempstart = temploc  # keep track of its ID for later
     templist = []   # make a list to store segments until you get to a dam
-    templist.append(temploc)  # seed the list with the
+    templist.append(temploc)  # seed the list with the initial segment
     ftemp = segments.loc[temploc, 'Frag']  # Fragment # of current segment
+    #fragments.loc[ftemp, 'Ndam'] += segments.loc[temploc, 'DamCount']
     print("Satarting headwater #", snum, "SegID", temploc,
           "damflag", ftemp)
 
@@ -88,10 +94,10 @@ while len(queue) > 0:
             print("Step", step, "Ending", temploc)
             ftemp = fexit+1  # New Fragment ID to be assigned
             # add an entry to the fragments DF
-            s = fragments.iloc[1]
+            """ s = fragments.iloc[1]
             s['flength'] = 0
             s.name = ftemp
-            fragments = fragments.append(s)
+            fragments = fragments.append(s) """
             fexit = fexit+1
             temploc = 0
 
@@ -109,6 +115,8 @@ while len(queue) > 0:
     # add the downstream segment to the end of the queue
     if temploc > 0:
         newstart = segments.loc[temploc, 'DnHydroseq']
+        #add to the count of dams
+        #fragments.loc[ftemp, 'Ndam'] += segments.loc[temploc, 'DamCount']
         if newstart in segments.index:
             queue = queue.append(segments.loc[newstart])
             print("Adding to Queue!", newstart)
@@ -121,15 +129,43 @@ while len(queue) > 0:
     
 # %%
 # Doing some summaries to cross check calculations
-print(fragments)
+#print(fragments)
 print(segments.loc[segments.Frag == 0]) #check for any segments not covered 
-print(segments.pivot_table('LENGTHKM', index='Frag', aggfunc=sum))
-print()
+temp = segments.pivot_table('LENGTHKM', index='Frag', aggfunc=sum)
+print(temp)
+print(sum(temp['LENGTHKM']))
+print(sum(segments.LENGTHKM))
 # alternate approach to pivot tabel 
 # segments.groupby('Frag')[['LENGTHKM']].sum()
 
 # %%
-# %matplotlib inline
+# Fill in fragment information
+#Total fragment length calculated using a pivot table from segment lengths
+fragments = segments.pivot_table('LENGTHKM', index='Frag', aggfunc=sum)
+
+#Join in the downstream segment for the segmane that contains the dam
+# Using a pivot table with a sum here since there should only be one 
+# value for a each non zero DamID and the zero will not be
+#included in the join
+frgDN=segments.pivot_table('DnHydroseq', index='DamID', aggfunc=sum)
+fragments = fragments.join(frgDN)
+
+# Use the downstream segment for each fragment to get its
+# downstream fragment ID
+fragments2 = fragments.merge(segments.Frag, left_on='DnHydroseq',  right_on='Hydroseq', suffixes=('_left', '_right'), how='left')
+fragments2.index = fragments.index
+fragments2.rename(columns={'Frag': 'FragDn'})
+
+# Mark fragments that are headwaters
+headlist = segments.loc[segments.UpHydroseq == 0, 'Frag'].unique()
+fragments2['HeadFlag'] = np.zeros(len(fragments2))
+fragments2.loc[headlist, 'HeadFlag'] = 1
+
+print(fragments2)
+
+#Next need to get aggregations by upstream area for fragments. 
+
+# %%
 print(segments.columns)
 segments['Frag'] = segments['Frag'].fillna(0)
 
