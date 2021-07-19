5e6bb9cccb3f166ccc2c749570366b35cfeaddf1
rspinti <rspinti@email.arizona.edu>
2021-07-19 16:00:34 -0700

    analysis reorg
    Reorganizing the analysis so it is more straight forward

--- archive/Rachel_testing/analysis_testing/dam_bar_plots.py
+++ archive/Rachel_testing/analysis_testing/dam_bar_plots.py
@@ -1,50 +1,77 @@
 #%%
 import geopandas as gp, pandas as pd, numpy as np, matplotlib.pyplot as plt
+from numpy.core.fromnumeric import size
 # %%
 ## Where to pull stuff from
 gdrive = "/Volumes/GoogleDrive/My Drive/Condon_Research_Group/Research_Projects/Rachel/Research/Data/bifurcation_data_repo/" 
-data_folder = "HPC_runs_fixed/processed_data/"
+data_folder = "final_analysis/processed_data/"
 
 ## Where to put stuff
-results_folder = "HPC_runs_fixed/analyzed_data/"
+results_folder = "final_analysis/analyzed_data/"
 
 ## The basins
-basin_ls = ['California', 'Colorado', 'Columbia', 'Great_Basin', 'Great_Lakes',
-'Gulf_Coast','Mississippi', 'North_Atlantic', 'Red', 'Rio_Grande','South_Atlantic']
+basin_ls = ['Great_Basin', 'Colorado', 'Rio_Grande', 'California', 'Gulf_Coast', 'Red', 
+'Mississippi', 'Columbia', 'South_Atlantic', 'Great_Lakes', 'North_Atlantic']
+# basin_ls = ['Great_Basin', 'Colorado', 'Rio_Grande', 'California', 'Gulf_Coast', 'Red',
+#  'Columbia', 'South_Atlantic', 'Great_Lakes', 'North_Atlantic']
 
 # basin_ls = ['California', 'Colorado']
 # basin = "California"
-
+basin_abr = ["GB", "CO", "RG", "CA", "GC", "RE", "MI", "CB", "SA", "GL", "NA"]
 # %%
-for basin in basin_ls:
+# Plotting
+c_nabd = ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#fff66f', '#d1ef77', '#79ce6b', '#5bbb9d', '#3288bd', '#3952aa', '#4f438e']
+# c_nabd = ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#fff66f', '#d1ef77', '#5bbb9d', '#3288bd', '#3952aa', '#4f438e']
+# # c_grand = ['#800035', '#992c39', '#cc5a38', '#d99453', '#e6de64', '#b3cc65', '#62a656', '#4a9980', '#287099', '#2e448c', '#383066']
+c_grand = ['#740030', '#842532', '#9a422a', '#98683a', '#afa94c', '#86994c', '#416e39', '#326857', '#1b4c69', '#213063', '#2a244e']
+# c_grand = ['#740030', '#842532', '#9a422a', '#98683a', '#afa94c', '#86994c', '#326857', '#1b4c69', '#213063', '#2a244e']
+# xlabels = ['Great Basin', 'Colorado', 'Rio Grande', 'California', 'Gulf Coast', 'Red',
+#  'Columbia', 'South Atlantic', 'Great Lakes', 'North Atlantic']
+
+# fig1 = plt.figure()
+fig1, ax1 = plt.subplots(1, 1, figsize=(15, 10))
+fig2, ax2 = plt.subplots(1, 1, figsize=(15, 10))
+for count, basin in enumerate(basin_ls):
     #GRanD
-    grand = pd.read_csv(gdrive+data_folder+"grand_dams/2010/"+basin+".csv", index_col='Hydroseq',
+    grand = pd.read_csv(gdrive+data_folder+"grand_data/2010/"+basin+".csv", index_col='Hydroseq',
                     usecols=['Hydroseq', 'DamCount','geometry', 'DamID',  'QC_MA', 'Norm_stor',
                                 'HUC2', 'HUC4', 'HUC8', 'Year_compl'])
     grand = grand[grand["DamID"]!=0]
 
     #NABD
-    nabd = pd.read_csv(gdrive+data_folder+"2010/"+basin+".csv", index_col='Hydroseq',
+    nabd = pd.read_csv(gdrive+data_folder+"nabd_data/2010/"+basin+".csv", index_col='Hydroseq',
                     usecols=['Hydroseq', 'DamCount','geometry', 'DamID',  'QC_MA', 'Norm_stor',
                                 'HUC2', 'HUC4', 'HUC8', 'Year_compl'])
     nabd = nabd[nabd["DamID"]!=0]
 
     ## Total # of dams
-    fig1 = plt.figure()
-    plt.bar("NABD",len(nabd), label="All dams", color = "black")
-    plt.bar("GRanD",len(grand), label = "Big dams", color = "#b63410")
-    plt.ylabel("Total number of dams")
-    plt.title(basin)
-    plt.legend()
-    plt.savefig(gdrive+results_folder+"2010/dam_bar_plots/number/"+basin+".png")
+    # ax1 = plt.subplot()
+    # ax1.bar(basin,len(nabd), label="All dams "+basin, color = c_nabd[count])
+    ax1.bar(basin,len(nabd), color = c_nabd[count])
+    # ax1.bar(basin,len(grand), label ="Big dams "+basin, color = c_grand[count])
+    ax1.bar(basin,len(grand), color = c_grand[count])
+    ax1.set_ylabel("Total number of dams", size=30)
+    ax1.set_xlabel("Basin", size=30)
+    ax1.set_xticklabels(basin_abr)
+    ax1.tick_params(axis = 'both', which = 'major', labelsize = 28)
+    #plt.legend()
+    # plt.show()
+    fig1.savefig(gdrive+results_folder+"bar_plots/number_dams.png")
 
     ## Total storage
-    fig2 = plt.figure()
-    plt.bar("NABD",(sum(nabd.Norm_stor)*1233.48)/(10**6), label="All dams", color = "black")
-    plt.bar("GRanD",(sum(grand.Norm_stor)*1233.48)/(10**6), label = "Big dams", color = "#b63410")
-    plt.ylabel("Total storage (MCM)")
-    plt.title(basin)
-    plt.legend()
-    plt.savefig(gdrive+results_folder+"2010/dam_bar_plots/storage/"+basin+".png")
+    ax2 = plt.subplot(111)
+    
+    ax2.bar(basin,(sum(nabd.Norm_stor)*1233.48)/(10**6), color = c_nabd[count])
+    ax2.bar(basin,(sum(grand.Norm_stor)*1233.48)/(10**6), color = c_grand[count])
+    ax2.set_ylabel("Total storage (MCM)", size=30)
+    ax2.set_xlabel("Basins", size=30)
+    ax2.set_xticklabels(basin_abr)
+    ax2.tick_params(axis='both', which='major', labelsize=28)
+    # plt.title(basin)
+    # plt.legend(bbox_to_anchor=(1, 0.7, 0.5, 0), fontsize=14)
+    plt.tight_layout()
+    # plt.legend(bbox_to_anchor=[1, 0.75, 0.2, 0])
+    # plt.show()
+    fig2.savefig(gdrive+results_folder+"bar_plots/dam_storage.png")
 
 # %%
--- archive/Rachel_testing/analysis_testing/frag_analysis.py
+++ archive/Rachel_testing/analysis_testing/frag_analysis.py
@@ -1,15 +1,16 @@
 #%%
 import geopandas as gp, pandas as pd, numpy as np, matplotlib.pyplot as plt, seaborn as sns
 from matplotlib.pyplot import cm
-sns.set()
+# sns.set()
+sns.set_style("darkgrid", {"axes.facecolor": ".8"})
 
 #%%
 #Folders and stuff to pull the data from
 gdrive = "/Volumes/GoogleDrive/My Drive/Condon_Research_Group/Research_Projects/Rachel/Research/Data/bifurcation_data_repo/" #where shapefiles/csv live
 
 ## NABD
-data_folder = 'HPC_runs_fixed/processed_data/'
-results_folder = 'HPC_runs_fixed/analyzed_data/'
+data_folder = 'final_analysis/processed_data/'
+results_folder = 'final_analysis/analyzed_data/len_analysis/'
 
 ### GRanD
 # data_folder = 'HPC_runs_fixed/processed_data/grand_dams/'
@@ -23,9 +24,10 @@ results_folder = 'HPC_runs_fixed/analyzed_data/'
 # basin_ls = ['California', 'Colorado']
 
 basin_ls = ['Great_Basin', 'Colorado', 'Rio_Grande', 'California', 'Gulf_Coast', 'Red', 'Mississippi', 'Columbia', 'South_Atlantic', 'Great_Lakes', 'North_Atlantic']
+# basin_ls = ['Great_Basin', 'Colorado', 'Rio_Grande', 'California', 'Gulf_Coast', 'Red', 'Columbia', 'South_Atlantic', 'Great_Lakes', 'North_Atlantic']
 #%%
 #Function to get info by fragment length
-def make_df(basin, years,  bin_ls):
+def make_df(basin, years,  bin_ls, gdrive, results_folder):
     bins = np.clip(bin_ls, bin_ls[0], bin_ls[-1])
     labels = [str(x) for x in bins[1:]]
 
@@ -38,11 +40,11 @@ def make_df(basin, years,  bin_ls):
     big_dams = pd.DataFrame(0, index=years, columns=labels)
 
     for year in years:
-        frags_nabd = pd.read_csv(gdrive+data_folder+year+"/"+basin+"_fragments.csv")
+        frags_nabd = pd.read_csv(gdrive+data_folder+"nabd_data/"+year+"/"+basin+"_fragments.csv")
         frag_nabd_bins = pd.cut(frags_nabd["LENGTHKM"], bins=bins, labels=labels)
         frag_nabd_bins.value_counts().sort_index()
 
-        frags_grand = pd.read_csv(gdrive+data_folder+"grand_dams/"+year+"/"+basin+"_fragments.csv")
+        frags_grand = pd.read_csv(gdrive+data_folder+"grand_data/"+year+"/"+basin+"_fragments.csv")
         frag_grand_bins = pd.cut(frags_grand["LENGTHKM"], bins=bins, labels=labels)
         frag_grand_bins.value_counts().sort_index()
 
@@ -61,83 +63,146 @@ def make_df(basin, years,  bin_ls):
             else:
                 nabd_diff.loc[year,value]=0
                 grand_diff.loc[year,value]=0
+    # nabd_basins.to_csv(results_folder+basin+"_nabd_basins.csv")
+    nabd_diff.to_csv(gdrive+results_folder+basin+"_nabd_frag_diff.csv")
+    # grand_basins.to_csv(results_folder+basin+"_grand_basins.csv")
+    grand_diff.to_csv(gdrive+results_folder+basin+"_grand_frag_diff.csv")
+    small_dams.to_csv(gdrive+results_folder+basin+"_small_dams_fraction.csv")
+    big_dams.to_csv(gdrive+results_folder+basin+"_big_dams_fraction.csv")
     return nabd_basins, nabd_diff, grand_basins, grand_diff, small_dams, big_dams
 
-# def make_tot_df(basin):
-#     years = ["no_dams", "1920", "1950", "1980", "2010"]
-#     bins = [0, 1, 10, 100, 1000, 10000, 100000, 1000000]
-#     # labels = [str(x) for x in bins[1:]]
-#     basin_df = pd.DataFrame(0, index=years, columns=["total_frags"])
+def make_tot_df(basin, years, gdrive, results_folder):
+    # years = ["no_dams", "1920", "1950", "1980", "2010"]
+    nabd_df = pd.DataFrame(0, index=years, columns=["total_frags"])
+    grand_df = pd.DataFrame(0, index=years, columns=["total_frags"])
+    small_dams = pd.DataFrame(0, index=years, columns=["total_frags"])
 
-#     for year in years:
-#         frags = pd.read_csv(gdrive+data_folder+year+"/"+basin+"_fragments.csv")
-#         basin_df.loc[year,"total_frags"]=len(frags["LENGTHKM"])
-    
-#     return basin_df
+    for year in years:
+        nabd_frags = pd.read_csv(gdrive+data_folder+"nabd_data/"+year+"/"+basin+"_fragments.csv")
+        nabd_df.loc[year,"total_frags"]=len(nabd_frags["LENGTHKM"])
+        grand_frags = pd.read_csv(gdrive+data_folder+"grand_data/"+year+"/"+basin+"_fragments.csv")
+        grand_df.loc[year,"total_frags"]=len(grand_frags["LENGTHKM"])
+
+        small_dams.loc[year, "total_frags"] = nabd_df.loc[year,"total_frags"]-grand_df.loc[year,"total_frags"]
+        percent_big = grand_df["total_frags"]/nabd_df["total_frags"]
+        small_dams["percent_small"] = 1-percent_big
+    small_dams["percent_small"]["no_dams"] = 0
+
+    nabd_df.to_csv(gdrive+results_folder+basin+"_nabd_totfrag.csv")
+    grand_df.to_csv(gdrive+results_folder+basin+"_grand_totfrag.csv")
+    small_dams.to_csv(gdrive+results_folder+basin+"_small_dams_totfraction.csv")
+    return nabd_df, grand_df, small_dams
 #%%
 #Plotting
 years = ["no_dams", "1920", "1950", "1980", "2010"]
 bin_ls = [0, 10, 100, 1000, 10000]
 lengths = [10, 100, 1000, 10000]
-# upper_limit=[2500, 18000, 4500, 800, 300, 30, 3]
+upper_limit=[18000, 4500, 800, 300]
+smaller_limit = [100, 250, 210, 150]
 xlabels =["Pre-development", "1920", "1950", "1980", "2010"]
+basin_abr = ["GB", "CO", "RG", "CA", "GC", "RE", "MI", "CB", "SA", "GL", "NA"]
 # lengths = [1]
 # c_old = ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#fee08b', '#ffffa3', '#c7f58c', '#abdda4', '#66c2a5', '#3288bd', '#5e4fa2']
-c = ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#ffffa3', '#c7f58c', '#abdda4', '#66c2a5', '#3288bd', '#3952aa', 'purple']   #'#4f438e'
+c = ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#fff66f', '#d1ef77', '#79ce6b', '#5bbb9d', '#3288bd', '#3952aa', '#4f438e']
 
-fig, axes = plt.subplots(4, 4, figsize=(20, 25))
-# for num, l in enumerate(lengths):
+# fig, axes = plt.subplots(4, 4, figsize=(20, 25))
+# pad=5
+# # # fig.text(0.09,0.5, "Number of fragments for all dams", ha="center", va="center", rotation=90, size = 14)
+# # # fig.text(0.3,0.5, "Number of fragments for big dams", ha="center", va="center", rotation=90, size = 14)
+
+# # for num, l in enumerate(lengths):
+# for count, basin in enumerate(basin_ls):
+#     df = make_df(basin, years, bin_ls, gdrive, results_folder)
+#     for row, l in enumerate(lengths):
+#         #NABD
+#         axes[row,0].plot(df[1].index, df[1][str(l)], label = basin, color=c[count], marker='o')
+#         # axes[row,0].set_ylim(0, upper_limit[row])
+#         axes[row,0].set_ylim(0, smaller_limit[row])
+#         # axes[row,0].set_title("Fragment length "+str(bin_ls[row])+" - "+str(l)+" km", weight="bold")
+#         axes[0,0].set_title("All dams", weight="bold")
+#         axes[row,0].set_xticklabels(xlabels)
+#         axes[row,0].set_ylabel("Number of fragments", weight="bold")
+#         axes[row,0].set_xlabel("Time", weight="bold")
+#         axes[row,0].annotate("Fragment length "+str(bin_ls[row])+" - "+str(l)+" km", xy=(0, 0.5), xytext=(-axes[row,0].yaxis.labelpad - pad, 0),
+#                 xycoords=axes[row,0].yaxis.label, textcoords='offset points',
+#                 size='large', ha='right', va='center')
+#         #GRanD
+#         axes[row,1].plot(df[3].index, df[3][str(l)], label = basin, color=c[count], marker='o')
+#         # axes[row,1].set_ylim(0, upper_limit[row])
+#         axes[row,1].set_ylim(0, smaller_limit[row])
+#         # axes[row,1].set_title("Fragment length "+str(bin_ls[row])+" - "+str(l)+" km", weight="bold")
+#         axes[0,1].set_title("Big dams", weight="bold")
+#         axes[row,1].set_xticklabels(xlabels)
+#         axes[row,1].set_ylabel("Number of fragments", weight="bold")
+#         axes[row,1].set_xlabel("Time", weight="bold")
+
+#         #Small dams
+#         axes[row,2].plot(df[4][1:].index, df[4][str(l)][1:], label = basin, color=c[count], marker='o')
+#         axes[row,2].set_ylim(0, 1)
+#         # axes[row,2].set_title("Fragment length "+str(bin_ls[row])+" - "+str(l)+" km", weight="bold")
+#         axes[0,2].set_title("Fraction of small dams", weight="bold")
+#         axes[row,2].set_xticklabels(xlabels[1:])
+#         axes[row,2].set_ylabel("Fraction of fragments", weight="bold")
+#         axes[row,2].set_xlabel("Time", weight="bold")
+
+#         #Large dams
+#         axes[row,3].plot(df[5].index[1:], df[5][str(l)][1:], label = basin, color=c[count], marker='o')
+#         axes[row,3].set_ylim(0, 1)
+#         axes[0,3].set_title("Fraction of big dams", weight="bold")
+#         axes[row,3].set_xticklabels(xlabels[1:])
+#         axes[row,3].set_ylabel("Fraction of fragments", weight="bold")
+#         axes[row,3].set_xlabel("Time", weight="bold")
+#         # axes[row,3].set_xticks(np.arange(0,len(xlabels)),labels=xlabels)
+# plt.tight_layout(rect=[0, 0, 0.94, 1])  
+# # plt.savefig(gdrive+results_folder+"frag_len4x4_smally.png", dpi=150)
+# # plt.savefig(gdrive+results_folder+"frag_len4x4_bigy.png", dpi=150)
+
+fig, axes = plt.subplots(1, 2, figsize=(25, 10))
 for count, basin in enumerate(basin_ls):
-    df = make_df(basin, years, bin_ls)
-    for row, l in enumerate(lengths):
-        #NABD
-        axes[row,0].plot(df[1].index, df[1][str(l)], label = basin, color=c[count], marker='o')
-        axes[row,0].set_title(str(l)+" km")
-        plt.xticks(np.arange(0,len(xlabels)),labels=xlabels)
-        #GRanD
-        axes[row,1].plot(df[3].index, df[3][str(l)], label = basin, color=c[count], marker='o')
-        axes[row,1].set_title(str(l)+"km")
-        # axes[row,1].set_xticks(np.arange(0,len(xlabels)),labels=xlabels)
-        #Small dams
-        axes[row,2].plot(df[4].index, df[4][str(l)], label = basin, color=c[count], marker='o')
-        axes[row,2].set_title(str(l)+" km")
-        # axes[row,2].set_xticks(np.arange(0,len(xlabels)),labels=xlabels)
-        #Large dams
-        axes[row,3].plot(df[5].index, df[5][str(l)], label = basin, color=c[count], marker='o')
-        axes[row,3].set_title(str(l)+" km")
-        # axes[row,3].set_xticks(np.arange(0,len(xlabels)),labels=xlabels)
-
-
-    # #NABD
-    # axes[0,0].plot(df[1].index, df[1][str(lengths[0])], label = basin, color=c[count], marker='o')
-    # axes[1,0].plot(df[1].index, df[1][str(lengths[1])], label = basin, color=c[count], marker='o')
-    # axes[2,0].plot(df[1].index, df[1][str(lengths[2])], label = basin, color=c[count], marker='o')
-    # axes[3,0].plot(df[1].index, df[1][str(lengths[3])], label = basin, color=c[count], marker='o')
-    # axes[0,0].set_title("All dams")
-    # axes[1,0].set_title("100km")
-    # axes[3,0].set_xlabel("Time")
-
-    # #GRanD
-    # axes[0,1].plot(df[3].index, df[3][str(lengths[0])], label = basin, color=c[count], marker='o')
-    # axes[1,1].plot(df[3].index, df[3][str(lengths[1])], label = basin, color=c[count], marker='o')
-    # axes[2,1].plot(df[3].index, df[3][str(lengths[2])], label = basin, color=c[count], marker='o')
-    # axes[3,1].plot(df[3].index, df[3][str(lengths[3])], label = basin, color=c[count], marker='o')
-    # axes[0,1].set_title("Big dams")
-
-    # #Small dams
-    # axes[0,2].plot(df[4].index, df[4][str(lengths[0])], label = basin, color=c[count], marker='o')
-    # axes[1,2].plot(df[4].index, df[4][str(lengths[1])], label = basin, color=c[count], marker='o')
-    # axes[2,2].plot(df[4].index, df[4][str(lengths[2])], label = basin, color=c[count], marker='o')
-    # axes[3,2].plot(df[4].index, df[4][str(lengths[3])], label = basin, color=c[count], marker='o')
-    # axes[0,2].set_title("Small dams percent")
-
-    # #Large dams
-    # axes[0,3].plot(df[5].index, df[5][str(lengths[0])], label = basin, color=c[count], marker='o')
-    # axes[1,3].plot(df[5].index, df[5][str(lengths[1])], label = basin, color=c[count], marker='o')
-    # axes[2,3].plot(df[5].index, df[5][str(lengths[2])], label = basin, color=c[count], marker='o')
-    # axes[3,3].plot(df[5].index, df[5][str(lengths[3])], label = basin, color=c[count], marker='o')
-    # axes[0,3].set_title("Big dams percent")
+    tot_df = make_tot_df(basin, years, gdrive, results_folder)
+    axes[0].plot(tot_df[0].index, tot_df[0]["total_frags"], label = basin_abr[count], color=c[count], marker='o')
+    axes[1].plot(tot_df[2].index[1:], tot_df[2]["percent_small"][1:], label = basin_abr[count], color=c[count], marker='o')
+    axes[1].set_ylim(0, 1)
     
+    # axes[1].plot(tot_df[0].index, tot_df[0]["total_frags"], label = basin, color=c[count], marker='o')
+    # axes[1].set_ylim(0, 5000)
+    # axes[2].plot(tot_df[1].index, tot_df[1]["total_frags"], label = basin, color=c[count], marker='o')
+    # axes[2].set_ylim(0, 5000)
+    # axes[3].plot(tot_df[2].index[1:], tot_df[2]["percent_small"][1:], label = basin, color=c[count], marker='o')
+    # axes[3].set_ylim(0, 1)
+
+    # axes[0].set_xlabel("Time", weight="bold")
+    # axes[1].set_xlabel("Time", weight="bold")
+    # axes[2].set_xlabel("Time", weight="bold")
+    # axes[3].set_xlabel("Time", weight="bold")
+
+    axes[0].set_ylabel("Total number of fragments", weight="bold", size=30)
+    # axes[1].set_ylabel("Total number of fragments", weight="bold")
+    # axes[2].set_ylabel("Total number of fragments", weight="bold")
+    axes[1].set_ylabel("Fraction of fragments", weight="bold", size=30)
+
+    axes[0].set_xticklabels(xlabels)
+    # axes[1].set_xticklabels(xlabels)
+    # axes[2].set_xticklabels(xlabels)
+    # axes[3].set_xlim('1920', '2012')
+    axes[1].set_xticklabels(xlabels[1:])
+
+    axes[0].set_title("All dams", weight="bold", size = 32)
+    # axes[1].set_title("All dams", weight="bold")
+    # axes[2].set_title("Big dams", weight="bold")
+    axes[1].set_title("Small dams", weight="bold", size=32)
+
+plt.tight_layout(rect=[0.05, 0, 0.8, 1])    
+# plt.legend(bbox_to_anchor=(1, 0.9, 0.5, 0), fontsize=20)
+# plt.legend(fontsize=28)
+axes[0].legend(loc="upper left", fontsize=28)
+axes[0].tick_params(axis = 'both', which = 'major', labelsize = 28)
+axes[1].tick_params(axis = 'both', which = 'major', labelsize = 28)
+# plt.legend(loc="upper left", fontsize=20)
+plt.savefig(gdrive+results_folder+"tot_frags1x2.png", dpi=150)
+plt.show()
+#%%
+# Old plotting
         # plt.plot(df[1].index, df[1][str(l)], label = basin, color=c[count], marker='o')
     #     plt.plot(df[1].index, df[1][str(l)], label = basin, color=c[count], marker='o', linestyle="--")
     #     plt.xlabel("Time")
